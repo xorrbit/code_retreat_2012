03fac4ff8e2af7f9849445e76bed0832a10f4c66
Andrew Orr <andrew@andreworr.ca>
2012-12-08 22:19:15 -0600

    Added Board implementations for tests

--- 3.cs
+++ 3.cs
@@ -1,9 +1,33 @@
 window.Board = class Board
   @state
   constructor: (myState...) ->
-    @state = myState
+    @state = myState.sort()
+  evolve: =>
+    possible_cells = []
+    for coords in @state
+      for xoff in [-1..1]
+        for yoff in [-1..1]
+          x = coords[0] + xoff
+          y = coords[1] + yoff
+          possible_cells.push([x, y]) if not (possible_cells.some (coord) -> (coord[0] == x and coord[1] == y))
+    alive_cells = []
+    for coords in possible_cells
+      alive_cells.push(coords) if this.cellAt(coords[0], coords[1]).willBeAliveNextTick()
+    new Board(alive_cells)
+  compare: (newBoard) =>
+    return @state.toString().localeCompare newBoard.state.toString()
+  cellAt: (x, y) =>
+    status = this.isAliveAt(x, y)
+    neighbors = this.countNeighboursForCellAt(x, y)
+    new Cell(status, neighbors)
   isAliveAt: (x, y) =>
     @state.some (coord) -> coord[0] == x and coord[1] == y
+  countNeighboursForCellAt: (x, y) =>
+    neighbours = 0
+    for xoff in [-1..1]
+      for yoff in [-1..1]
+        neighbours++ if not (xoff == yoff == 0) and this.isAliveAt(x + xoff, y + yoff)
+    neighbours
 
 window.Cell = class Cell
   @liveNeighbors
